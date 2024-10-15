8c675ecfc461ff568bf58cd92def47838831d674
samiecode <samiekeyz02@gmail.com>
2024-10-15 21:07:37 +0100

    chore: refactor root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
-import {useAccount, useConnect, useDisconnect} from "wagmi";
 import LandingPage from "@/components/home";
 
-function App() {
-	const account = useAccount();
-	const {connectors, connect, status, error} = useConnect();
-	const {disconnect} = useDisconnect();
-
-	return (
-		<>
-			<LandingPage />
-		</>
-	);
+export default function Home() {
+	return <LandingPage />;
 }
-
-export default App;
