15b6a0ecdce53dc0e0481ab634a662a40e326007
samiecode <samiekeyz02@gmail.com>
2024-10-15 20:57:05 +0100

    chore: remove interface from dashboard-layout

--- src/app/dashboard/(overview)/layout.tsx
+++ src/app/dashboard/(overview)/layout.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import SideBar from "@/components/dashboard/SideBar";
 
-interface DashboardLayoutProps {
-	children: React.ReactNode;
-}
-
-const DashBoardLayout: React.FC<DashboardLayoutProps> = ({children}) => {
+export default function DashBoardLayout({
+	children,
+}: Readonly<{children: React.ReactNode}>) {
 	return (
 		<main className="w-full md:h-screen md:max-h-screen overflow-hidden grid grid-cols-1 grid-rows-[70px_1fr] md:grid-rows-1 md:grid-cols-[250px_1fr] relative">
 			<section className="col-span-1 h-full relative">
@@ -17,5 +15,4 @@ const DashBoardLayout: React.FC<DashboardLayoutProps> = ({children}) => {
 			</section>
 		</main>
 	);
-};
-export default DashBoardLayout;
+}
